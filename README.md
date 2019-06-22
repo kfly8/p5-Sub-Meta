c3aae34e7e90f974877b2fe35fd56b6631d56e24
kfly8 <kfly@cpan.org>
2019-06-22 03:52:51 -0400

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Sub::Meta - It's new $module
+
+# SYNOPSIS
+
+    use Sub::Meta;
+
+# DESCRIPTION
+
+Sub::Meta is ...
+
+# LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kfly8 <kfly@cpan.org>
---
+++ lib/Sub/Meta.pm
@@ -0,0 +1,39 @@
+package Sub::Meta;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sub::Meta - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sub::Meta;
+
+=head1 DESCRIPTION
+
+Sub::Meta is ...
+
+=head1 LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kfly8 E<lt>kfly@cpan.orgE<gt>
+
+=cut
+
